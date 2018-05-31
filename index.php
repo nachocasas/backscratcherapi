601fff75b2577359dc50ceb84bd1a410576f0df5
Ignacio Casas <igdcasas@gmail.com>
2018-05-31 14:45:33 -0300

    change procfile

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__.'/service/ScratcherService.php';
 require_once __DIR__.'/lib/Auth.php';
 
