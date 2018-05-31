a60d0ea7e8406863fd45a39fa537c4f13ddb8456
Ignacio Casas <igdcasas@gmail.com>
2018-05-31 18:12:18 -0300

    add header for preflight request

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization");
 
 require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__.'/service/ScratcherService.php';
